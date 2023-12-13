5fab88b5a335fbbc12c91cfb5e1f50f1e072b70b
илюха сакура <144677654+overterebonka52@users.noreply.github.com>
2023-12-13 21:59:02 +0300

    Add files via upload

---
+++ dkr2z1.pas
@@ -0,0 +1,41 @@
+﻿program dkr2z1;
+const
+  r = 10; // размер массива
+var
+  arr: array[1..r] of integer; 
+  k: integer; // к-ая статистика
+  i, j, x: integer;
+
+begin
+  writeln('Введите массив: '); // ввод массива с клавиатуры
+  for i := 1 to r do
+    read(arr[i]);
+  
+  write('Введите k: '); // ввод k
+  readln(k);
+  
+  if (k < 1) or (k > r) then // проверка корректности введенного k
+  begin
+    writeln('Ошибка: k должно быть от 1 до ', r);
+    exit;
+  end;
+  
+  for i := low(arr) to high(arr) do // алгоритм поиска k-ой статистики
+  begin
+    x := arr[i];
+    j := i;
+    while (j > 1) and (x < arr[j - 1]) do
+    begin
+      arr[j] := arr[j - 1];
+      j := j - 1; // уменьшает j на единицу
+    end;
+    arr[j] := x;
+  end;
+  
+  write('Отсортированный массив: '); // вывод отсортированного массива
+  for i := low(arr) to high(arr) do
+    write(arr[i], ' ');
+  writeln;
+  
+  writeln('K-ая статистика: ', arr[k]); // вывод k-ой статистики
+end.
---
+++ dkr2z2.pas
@@ -0,0 +1,31 @@
+﻿program dkr2z2;
+var
+  s: string;
+  i, dl, j, z: integer;
+  n: Boolean;
+begin
+  writeln('Введите строку');
+  readln(s);
+  z:=0;
+  dl:=length(s);
+  for i:=0 to dl-3 do
+  begin
+    j:=0;
+    n:=false;
+    while j<dl-3 do
+    begin
+      if (i+1=j+1) and (i+2=j+2) and (i+3=j+3) then 
+        j:=j+1;
+      if (s[dl-2]=s[i+1]) and (s[dl-1]=s[i+2]) 
+            and (s[dl]=s[i+3]) and (i+1<>dl-2) and (i+2<>dl-1) and (i+3<>dl) then
+        n:=true;
+      if (s[i+1]=s[j+1]) and (s[i+2]=s[j+2]) and (s[i+3]=s[j+3]) then
+        n:=true;
+      j:=j+1;
+    end;
+    if n=false then 
+      z:=z+1;
+  end;
+  write('Количество уникальных подстрок из трёх символов: ', z)
+end.
+
